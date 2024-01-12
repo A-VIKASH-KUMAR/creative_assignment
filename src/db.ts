2e7a45b6084b94b34f70d14296b290b0d65bfa0a
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-01-12 17:20:20 +0530

    feat: :sparkles: add db.ts file

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1", {
-        dbName:process.env.MONGO_DB
+        dbName:process.env.MONGO_DB ?? "test"
     });
 
     const db = mongoose.connection;
