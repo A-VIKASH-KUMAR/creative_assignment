d89645d5d6a5b59b4cba6c8ab9269e05d256c47a
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-01-12 15:37:09 +0530

    feat: :sparkles: add mongodb connection file

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1", {
+        dbName:process.env.MONGO_DB
+    });
+
+    const db = mongoose.connection;
+
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    db.once("open", () => {
+      console.log("Connected to MongoDB");
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("unable  to connect to mongodb", error);
+  }
+};
+
+export { connectDb };
